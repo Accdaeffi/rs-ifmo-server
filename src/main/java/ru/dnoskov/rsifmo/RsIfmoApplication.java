1e4b454e98c576238cdc5bf68cea98f773ffe56c
Accdaeffi <noskoff2@yandex.ru>
2022-04-01 16:12:15 +0300

    updated to deploying on glassfish

--- src/main/java/ru/dnoskov/rsifmo/RsIfmoApplication.java
+++ src/main/java/ru/dnoskov/rsifmo/RsIfmoApplication.java
@@ -2,16 +2,24 @@ package ru.dnoskov.rsifmo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.context.WebApplicationContext;
 
 import ru.dnoskov.rsifmo.webservice.services.rest.PersonReadService;
 
 @SpringBootApplication
-public class RsIfmoApplication {
+public class RsIfmoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		WebApplicationContext ctx = (WebApplicationContext) SpringApplication.run(RsIfmoApplication.class, args);
 		ctx.getBean(PersonReadService.class);
 	}
+        
+        
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RsIfmoApplication.class);
+    }
 
 }
--- src/main/java/ru/dnoskov/rsifmo/webservice/database/ConnectionUtils.java
+++ src/main/java/ru/dnoskov/rsifmo/webservice/database/ConnectionUtils.java
@@ -3,6 +3,9 @@ package ru.dnoskov.rsifmo.webservice.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.annotation.Resource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import javax.sql.DataSource;
 
@@ -10,23 +13,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ConnectionUtils {
-	private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/ifmo-ws";
-	private static final String JDBC_USER = "ifmo-ws";
-	private static final String JDBC_PASSWORD = "ifmo-ws";
 
-	DataSource dataSource;
+    private DataSource dataSource;
+    
+    static ConnectionUtils instance = null;
+        
+    private ConnectionUtils() {
+    	try {
+    		InitialContext context = new InitialContext();
+    		dataSource = (DataSource) context.lookup("jdbc/ifmo-ws");
+                
+     	} catch (NamingException ex) {
+            ex.printStackTrace();
+        }
+            
+    }
 	
-	public Connection getConnection() {
-		Connection connection = null;
+    public Connection getConnection() {
+        Connection result = null;
 		
-		try {
-			connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
-		} catch (SQLException ex) {
-			System.out.println("Exception!");
-			System.out.println(ex);
-			// log.error("No database connection!");
-		}
-		return connection;
-	 }
+	try {
+            result = dataSource.getConnection();
+	} catch (SQLException ex) {
+            System.out.println("Exception!");
+            System.out.println(ex);
+	}
+            return result;
+    }
 
 }
